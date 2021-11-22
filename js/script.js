36d6e18bad8aef7755fff198626bde3d6588d43d
masterng <masterng@yandex.ru>
2021-11-22 15:15:00 +0500

    сделал адаптивное меню для планшетной версии

--- js/script.js
+++ js/script.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
         speed: 2000,
         autoplaySpeed: 3000
     });
+
+    $('.top_menu_burger').click(function(event) {
+        $('.top_header_nav, .top_logo, .top_menu_burger, .top_nav-menu').toggleClass('active');
+    });
+
+    $('.bottom_menu_burger').click(function(event) {
+        $('.bottom_header_nav, .bottom_logo, .bottom_menu_burger, .bottom_nav-menu').toggleClass('active');
+        $('.bottom_menu_burger_text, .copywhrite').toggleClass('display_none');
+    });
 });
