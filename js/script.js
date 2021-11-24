2ca399465efcdb14e8f8cffd964d539acd98e701
masterng <masterng@yandex.ru>
2021-11-24 09:49:19 +0500

    закончил планшетную версию

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ $(document).ready(function() {
     });
 
     $('.top_menu_burger').click(function(event) {
-        $('.top_header_nav, .top_logo, .top_menu_burger, .top_nav-menu, header_nav').toggleClass('active');
+        $('.top_header_nav, .top_logo, .top_menu_burger, .top_nav-menu').toggleClass('active');
     });
 
     $('.bottom_menu_burger').click(function(event) {
-        $('.bottom_header_nav, .bottom_logo, .bottom_menu_burger, .bottom_nav-menu').toggleClass('active');
-        $('.bottom_menu_burger_text, .copywhrite').toggleClass('display_none');
+        $('.bottom_header_nav, .bottom_logo, .bottom_menu_burger, .bottom_nav-menu, .footer').toggleClass('active');
+        $('.bottom_menu_burger_text, .copywhrite, bottom_menu_burger_text').toggleClass('display_none');
     });
 });
